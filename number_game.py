5a318335901f16589a05b402564d612682a530b8
[nozomi] <[nozomi.belltree@gmail.com]>
2026-06-17 23:17:36 +0900

    add game loop

--- number_game.py
+++ number_game.py
@@ -5,16 +5,18 @@ answer = random.randint(100, 999)
 print("数字当てゲーム！！")
 print("100から999までの数字を当ててください")
 
-guess = int(input("数字を入力してください: "))
-
-if guess == answer:
-    print("正解です！")
-else:
-    print("不正解です")
-
-    if guess < answer:
-        print("もっと大きい数字です。")
+while True:
+    guess = int(input("数字を入力してください: "))
+    
+    if guess == answer:
+        print("正解です！")
+    
     else:
-        print("もっと小さい数字です。")
+        print("不正解です")
         
-    print(f"正解は {answer} でした。")
+        if guess < answer:
+            print("もっと大きい数字です。")
+            
+        else:
+            print("もっと小さい数字です。")
+            print(f"正解は {answer} でした。")
